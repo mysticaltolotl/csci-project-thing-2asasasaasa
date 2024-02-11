27d037c678100ad18cc99a202bd9e7127ae2ec83
mysticaltolotl <mysticaxolotl7@gmail.com>
2024-02-10 18:26:39 -0700

    Attempt to implement main()

--- app_1/main_1.cpp
+++ app_1/main_1.cpp
@@ -18,6 +18,122 @@ int main(int argc, char* argv[]) {
     // DO NOT MODIFY ABOVE.
 
     // TODO
+  
+  	//define common vars
+  	string newURL, prevURL, owner;
+    int id, option;
+    Webpage* temp;
+    
+  	displayMenu();
+    cin >> option;
+  
+  	bool built = false; // there's probably a more elegant way to see if a list has been built, but this works the same
+    bool valid = false;
+    bool looping = true;
+  
+  	BrowserHistory history;
+    
+    while(looping)
+    {
+        displayMenu();
+        cin >> option;
+        if(option == "1")
+
+        {
+            history.buildBrowserHistory();
+            history.displayHistory();
+            built = true;
+        }
+
+        else if(option == "2")
+        {
+            if(built)
+            {
+                history.displayHistory();
+            }
+            else
+            {
+            cout << "== CURRENT BROWSER HISTORY ==" << endl << "NULL" << endl << "===" << endl;
+            }
+        }
+
+        else if(option == "3")
+        {
+            cout << "Enter the new web page's url:" << endl;
+            cin >> newURL;
+            cout << "Enter the new web page's id:" << endl;
+            cin >> id;
+            cout << "Enter the previous page's url (or First):" << endl;
+            cin >> prevURL;
+            
+            if(history.searchPageByURL(newURL) != NULL)
+            {
+                return; //return if the URL is already taken
+            }
+            if(prevURL == "First")
+            {
+                int prevID = 10;
+            }
+            else
+            {
+                temp = history.searchPageByURL(prevURL);
+                int prevID = prev->id;
+            }
+            cout << "adding: [" << id << "]-" << newURL << " (prev: [" << prevID <<"])";
+            
+            history.updateViews(newURL);
+        }
+    
+        else if(option == "4")
+        {
+            valid = false;
+            while(!valid)
+            {
+                cout << "Enter url of the web page to add the owner:" << endl;
+                cin >> newURL;
+                cout << "Enter the owner:";
+                cin >> owner;
+
+                history.addOwner(newURL, owner);
+                temp = history.searchPageByURL(newURL);
+                
+                if(temp != NULL)
+                {
+                cout << "The owner (" << owner << ") has been added for the ID - " << temp->id << endl;
+                valid = true;
+                }
+                
+                cout << "Page not found. Try again." << endl;
+
+            }
+        }
+
+        else if(option == "5")
+        {
+            cout << "Enter url of the web page to check the view count: " << endl;
+            cin >> newURL;
+            temp = history.searchPageByURL(newURL);
+            if(temp != NULL)
+            {
+                cout << "View count for URL - " << newURL << "is " << temp->views << endl;
+            }
+            else
+            {
+                cout << "View count for URL - " << newURL << "is 0" << endl;
+            }
+        }
+
+        else if(option == "6")
+        {
+            cout << "Quitting...Goodbye!\n";
+            looping = false;
+        }
+
+        else
+        {
+            cout << "Invalid selection, please try again" << endl;
+        }
+    }
     
     return 0;
 }
